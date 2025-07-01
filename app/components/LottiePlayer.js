75209ecf477115e0579f9f0bb159d4d57cd36033
Tarun Benakanakonda <120493494+tarunmb@users.noreply.github.com>
2025-07-01 22:23:11 +0530

    Create LottiePlayer.js

---
+++ app/components/LottiePlayer.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+
+export default function LottiePlayer({ animationData }) {
+  const container = useRef(null);
+
+  useEffect(() => {
+    const anim = lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+    });
+
+    return () => anim.destroy();  // Clean up
+  }, [animationData]);
+
+  return (
+    <div ref={container} style={{ width: '100%', height: '100%' }} />
+  );
+}
