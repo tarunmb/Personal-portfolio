4499a7f45e6dff63e48c96d1db569aa5e2dc9c33
Tarun Benakanakonda <120493494+tarunmb@users.noreply.github.com>
2025-07-01 22:28:07 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,10 +1,10 @@
 export const contactsData = {
-    email: 'tarunbenakanakonda@gmail.com',
-    phone: '+91 9849000000',
-    address: 'Hyderabad, Telangana, India - 500001 ',
-    github: 'https://github.com/tarunbenakanakonda',
+    email: 'tarunmb2003@gmail.com',
+    phone: '+91 9538663780',
+    address: 'Bengaluru, Karnataka, India - 560098 ',
+    github: 'https://github.com/tarunmb',
     facebook: 'https://www.facebook.com/tarunbenakanakonda/',
-    linkedIn: 'https://www.linkedin.com/in/tarunbenakanakonda/',
+    linkedIn: 'https://www.linkedin.com/in/tarun-benakanakonda/',
     twitter: 'https://twitter.com/tarunbenakanakonda',
     stackOverflow: 'https://stackoverflow.com/users/16840768/tarunbenakanakonda',
     devUsername: "tarunbenakanakonda"
