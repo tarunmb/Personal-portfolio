6027ef764c5ef409f7b612a99acf5cad5c5d5ac8
Tarun Benakanakonda <120493494+tarunmb@users.noreply.github.com>
2025-07-02 00:23:24 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
