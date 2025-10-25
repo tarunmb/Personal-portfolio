734f76e229a419080d9ebedaee4a6d45ab690c08
Tarun Benakanakonda <120493494+tarunmb@users.noreply.github.com>
2025-10-25 20:14:20 +0530

    Fix

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import { useEffect, useState } from "react";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
