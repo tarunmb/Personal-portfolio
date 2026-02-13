e0d19fa25719056e69d9e908175732ff20757367
Tarun Benakanakonda <120493494+tarunmb@users.noreply.github.com>
2026-02-13 20:26:44 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 "use client";
 import { useEffect, useState } from "react";
-import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+import dynamic from "next/dynamic";
+
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
 
 export default function Home() {
   const [blogs, setBlogs] = useState([]);
